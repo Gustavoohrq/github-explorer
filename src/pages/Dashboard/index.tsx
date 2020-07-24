9c4b6a0b3ac94fa33e746f09310568855f912ab9
Gustavo Henrique <gustavofmariano@gmail.com>
2020-07-24 17:19:30 -0300

    add seacr repository

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,62 +1,58 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
+
+import api from '../../services/api'
 
 import { FiChevronRight } from 'react-icons/fi'
 import { Title, Form, Repositories } from './styles'
 import logoImg from '../../assets/logo.svg'
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [respositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>, ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`)
+    const respository = response.data;
+    setRepositories([...respositories, respository] )
+    setNewRepo('')
+
+  }
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
       <Title>Explore repositórios no Github</Title>
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do repositório"/>
-        <button>Pesquisar</button>
+      <Form onSubmit={handleAddRepository}>
+        <input
+        value={newRepo}
+        onChange={(e) => setNewRepo(e.target.value)}
+        placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
 
       </Form>
       <Repositories >
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
+        {respositories.map(respository => (
+          <a key={respository.full_name} href="teste">
+          <img src={respository.owner.avatar_url} alt={respository.owner.login}/>
 
           <div>
-            <strong>rockeseat / unform</strong>
-            <p>Lorem ipsum</p>
+            <strong>{respository.full_name}</strong>
+            <p>{respository.description}</p>
           </div>
           <FiChevronRight size={20} />
-
         </a>
 
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
+        ))}
 
-          <div>
-            <strong>rockeseat / unform</strong>
-            <p>Lorem ipsum</p>
-          </div>
-          <FiChevronRight size={20} />
-
-        </a>
-
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
-
-          <div>
-            <strong>rockeseat / unform</strong>
-            <p>Lorem ipsum</p>
-          </div>
-          <FiChevronRight size={20} />
-
-        </a>
-
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
-
-          <div>
-            <strong>rockeseat / unform</strong>
-            <p>Lorem ipsum</p>
-          </div>
-          <FiChevronRight size={20} />
-
-        </a>
       </Repositories>
 
     </>
