aad5b4d73a819a6ce12c0a34224edaaddaaccf1a
Gustavo Henrique <gustavofmariano@gmail.com>
2020-07-24 16:09:06 -0300

    add styled components and style global

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import {Title} from './styles'
 const Dashboard: React.FC = () => {
   return (
-    <h1>Dashboard</h1>
+    <Title>Explore repositórios no Github</Title>
   )
 };
 
