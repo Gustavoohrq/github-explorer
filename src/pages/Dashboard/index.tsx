4bebf367fc45b06cd4aeeed9c46efa6ff5a17458
Gustavo Henrique <gustavofmariano@gmail.com>
2020-07-24 16:46:50 -0300

    style home

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,65 @@
 import React from 'react'
-import {Title} from './styles'
+
+import { FiChevronRight } from 'react-icons/fi'
+import { Title, Form, Repositories } from './styles'
+import logoImg from '../../assets/logo.svg'
 const Dashboard: React.FC = () => {
   return (
-    <Title>Explore repositórios no Github</Title>
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório"/>
+        <button>Pesquisar</button>
+
+      </Form>
+      <Repositories >
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
+
+          <div>
+            <strong>rockeseat / unform</strong>
+            <p>Lorem ipsum</p>
+          </div>
+          <FiChevronRight size={20} />
+
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
+
+          <div>
+            <strong>rockeseat / unform</strong>
+            <p>Lorem ipsum</p>
+          </div>
+          <FiChevronRight size={20} />
+
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
+
+          <div>
+            <strong>rockeseat / unform</strong>
+            <p>Lorem ipsum</p>
+          </div>
+          <FiChevronRight size={20} />
+
+        </a>
+
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/47986830?s=460&u=d44a5fd4ef0d4e943315daf38c8efb7e438b12f8&v=4" alt="Gustavo Henrique"/>
+
+          <div>
+            <strong>rockeseat / unform</strong>
+            <p>Lorem ipsum</p>
+          </div>
+          <FiChevronRight size={20} />
+
+        </a>
+      </Repositories>
+
+    </>
   )
 };
 
