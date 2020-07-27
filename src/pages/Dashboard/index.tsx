105de9a6494338d1fb3490f330e191b33cdfbab8
Gustavo Henrique <gustavofmariano@gmail.com>
2020-07-27 18:05:31 -0300

    finish

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, FormEvent } from 'react'
-
+import React, { useState, FormEvent, useEffect } from 'react'
 import api from '../../services/api'
-
+import { Link } from 'react-router-dom'
 import { FiChevronRight } from 'react-icons/fi'
 import { Title, Form, Repositories, Error } from './styles'
 import logoImg from '../../assets/logo.svg'
@@ -18,7 +17,18 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
   const [inputError, setInputError] = useState('');
-  const [respositories, setRepositories] = useState<Repository[]>([]);
+  const [respositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplore:repositories');
+    if(storagedRepositories){
+      return JSON.parse(storagedRepositories);
+    };
+    return [];
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem('@GithubExplore:repositories', JSON.stringify(respositories))
+  }, [respositories]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>,): Promise<void> {
     event.preventDefault();
@@ -36,7 +46,8 @@ const Dashboard: React.FC = () => {
       setInputError('Erro na busca por esse repositório.');
     }
 
-  }
+  };
+
   return (
     <>
       <img src={logoImg} alt="GitHub Explorer" />
@@ -52,7 +63,7 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
       <Repositories >
         {respositories.map(respository => (
-          <a key={respository.full_name} href="teste">
+          <Link key={respository.full_name} to={`/repositories/${respository.full_name}`}>
             <img src={respository.owner.avatar_url} alt={respository.owner.login} />
 
             <div>
@@ -60,7 +71,7 @@ const Dashboard: React.FC = () => {
               <p>{respository.description}</p>
             </div>
             <FiChevronRight size={20} />
-          </a>
+          </Link>
 
         ))}
 
